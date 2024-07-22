acec8ced3feb56fb4d4a71a4c356dca8f06a452f
Kirill Ardyshev <kirill.arda@yandex.ru>
2024-07-23 01:47:25 +0500

    Добавление точек входа на график. (Требует правки)

--- scr/data/check_prepared_data.py
+++ scr/data/check_prepared_data.py
@@ -3,7 +3,6 @@ import pandas as pd
 import mplfinance as mpf
 import matplotlib.pyplot as plt
 
-
 class CheckPreparedData():
     def __init__(self):
         pass
@@ -15,14 +14,38 @@ class CheckPreparedData():
         df['Volume'] = df['Quote asset volume']
         df.set_index('Open time', inplace=True)
 
-        # Ensure that the dataframe has the required columns for mplfinance
-        # The columns should typically be 'Open', 'High', 'Low', 'Close', and optionally 'Volume'
+        def _convect_target(x):
+            if x > 0:
+                return 1
+            if x == 0:
+                return 0
+            if x < 0:
+                return -1
+
+        df['Forecast'] = df['target_predict'].apply(_convect_target)
+
+        # Создаем столбцы для сигналов вверх и вниз и заполняем NaN
+        df['Up Signal'] = float('nan')
+        df['Down Signal'] = float('nan')
+
+        # Заполняем столбцы значениями сигналов
+        df.loc[df['Forecast'] == 1, 'Up Signal'] = df['High']
+        df.loc[df['Forecast'] == -1, 'Down Signal'] = df['Low']
+
+        # Проверяем, содержат ли столбцы ненулевые значения
+        up_signals = df['Up Signal'].dropna().values
+        down_signals = df['Down Signal'].dropna().values
+
+        apdict = []
+        if len(up_signals) > 0:
+            apdict.append(mpf.make_addplot(df['Up Signal'], type='scatter', marker='^', markersize=100, color='g', panel=0))
+        if len(down_signals) > 0:
+            apdict.append(mpf.make_addplot(df['Down Signal'], type='scatter', marker='v', markersize=100, color='r', panel=0))
 
         # Создание графика японских свечей
-        fig, ax = mpf.plot(df, type='candle', style='charles', volume=True, returnfig=True, figscale=1.5, figsize=(60, 10))
+        fig, ax = mpf.plot(df, type='candle', style='charles', addplot=apdict, volume=True, returnfig=True, figscale=1.5, figsize=(60, 10))
 
         plt.show()
-
 
 if __name__ == '__main__':
     checker = CheckPreparedData()
