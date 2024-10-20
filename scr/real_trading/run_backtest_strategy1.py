50c59ef8feebf7f2e4fcc35ef4d4ac7c1642a12b
Kirill Ardyshev <kirill.arda@yandex.ru>
2024-10-20 22:17:10 +0500

    Тест "основной" стратегии.

---
+++ scr/real_trading/run_backtest_strategy1.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+import logging as logger
+from scr.real_trading.backtester_strategy1 import CustomStrategy
+from scr.data.data_preparation import DataPreparation
+
+
+class BacktestStrategy1():
+    def __init__(self):
+        pass
+
+    def _set_work_path(self) -> Path:
+        # Получаем текущий рабочий каталог
+        current_directory = os.getcwd()
+
+        # Ищем позицию, где находится "su-data-platform" в пути
+        index = current_directory.rfind("search_anomaly_Bitcoin")
+        index1 = current_directory.rfind("pysetup")
+
+        # Если "su-data-platform" найден, переходим к его родительскому каталогу
+        if index != -1:
+            project_directory = current_directory[: index + len("search_anomaly_Bitcoin")]
+
+            # Переходим в родительский каталог "su-data-platform"
+            os.chdir(project_directory)
+        elif index1 != -1:
+            project_directory = current_directory[: index1 + len("pysetup")]
+
+            # Переходим в родительский каталог "su-data-platform"
+            os.chdir(project_directory)
+        else:
+            raise Exception("Произошла ошибка задания рабочего каталога.")
+
+        current_directory = os.getcwd()
+        print(f"Рабочая директория: {current_directory}")
+        return Path(current_directory)
+
+
+    def run_backtest(self):
+        self._set_work_path()
+        df = DataPreparation().read_final_file()
+        print('df', type(df), list(df))
+
+
+if __name__ == '__main__':
+    run_test = BacktestStrategy1()
+    run_test.run_backtest()
+
